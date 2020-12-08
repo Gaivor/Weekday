cbe0cf47fd0cd2d5143fa9e2cf8f8da1b589b1b6
Ilia <gaivor1@gmail.com>
2020-12-08 18:01:34 +0300

    Add files via upload
    UI build

---
+++ Weekday/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Weekday
+//
+//  Created by Илья Гайворонский on 08.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var dateTextField: UITextField!
+    @IBOutlet weak var monthTextField: UITextField!
+    @IBOutlet weak var yearTextFiled: UITextField!
+    
+    @IBOutlet weak var resultTextField: UILabel!
+    
+    
+    @IBAction func findDay(_ sender: UIButton) {
+    }
+}
+
